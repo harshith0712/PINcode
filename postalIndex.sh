1a801c7a82114a79e4cf7ee3bd5e9f2c337f2ebf
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 19:06:15 +0530

    master

--- postalIndex.sh
+++ postalIndex.sh
@@ -2,4 +2,13 @@
 
 echo "Welcome To Postal Index Number Problem"
 
+echo "enter the Postal Index Number"
+read PIN
+pinPattern="[0-9]{6}|[0-9]{3}[[:space:]][0-9]{3}"
+   if [[ $PIN =~ $pinPattern ]]
+   then
+      echo "valid"
+   else
+      echo "not valid"
+   fi
 
