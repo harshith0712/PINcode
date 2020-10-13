313accce9736df1a237e3644bc104797eb478542
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 19:00:23 +0530

    usecase2

--- postalIndex.sh
+++ postalIndex.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "Welcome To Postal Index Number Problem"
+#!/bin/bash
+
+echo "enter the Postal Index Number"
+read PIN
+pinPattern="[0-9]{6}"
+   if [[ $PIN =~ $pinPattern ]]
+   then
+      echo "valid"
+   else
+      echo "not valid"
+   fi
 
 
