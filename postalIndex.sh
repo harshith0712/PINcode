cd6067b61d5d57711375deeb3cd442067d415adb
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-14 10:02:27 +0530

    usecase3

--- postalIndex.sh
+++ postalIndex.sh
@@ -3,7 +3,7 @@
 
 echo "enter the Postal Index Number"
 read PIN
-pinPattern="[0-9]{6}|[0-9]{3}[[:space:]][0-9]{3}"
+pinPattern="[0-9]{6}"
    if [[ $PIN =~ $pinPattern ]]
    then
       echo "valid"
